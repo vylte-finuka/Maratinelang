8c3d6f50b4cdc94c035b8c0f96266afd99e9774b
Abhina Sreeskantharajan <Abhina.Sreeskantharajan@ibm.com>
2026-05-28 15:16:49 -0400

    do not convert character by character

--- clang/lib/Lex/TextEncoding.cpp
+++ clang/lib/Lex/TextEncoding.cpp
@@ -42,7 +42,7 @@ TextEncoding::setConvertersFromOptions(TextEncoding &TEC,
     return ErrorOrConverter.getError();
 
   ErrorOrConverter = llvm::TextEncodingConverter::create(
-      TInfo.getDefaultNarrowTextEncoding(), UTF8);
+      TInfo.getDefaultOrdinaryTextEncoding(), UTF8);
 
   if (ErrorOrConverter)
     TInfo.FormatStrConverter =
