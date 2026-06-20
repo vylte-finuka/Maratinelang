5a217a9e7ac13f257f74583b02ed5875c8a7dbae
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2026-06-21 01:30:21 +0400

    Updating MaratineCodeGen.cpp.

--- maratine/lib/MaratineCodeGen.cpp
+++ maratine/lib/MaratineCodeGen.cpp
@@ -18,6 +18,7 @@ CodeGenerator::CodeGenerator(StringRef ModuleName, StringRef Arch)
   // Triple et DataLayout selon l'architecture cible
   if (Arch == "x64") {
     Mod->setTargetTriple(llvm::Triple("x86_64-pc-windows-msvc"));
+    Mod->setTargetTriple(llvm::Triple("x86_64-unknown-linux-gnu"));
     Mod->setDataLayout(
         "e-m:w-p270:32:32-p271:32:32-p272:64:64"
         "-i64:64-i128:128-f80:128-n8:16:32:64-S128");
